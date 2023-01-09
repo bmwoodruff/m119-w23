5d6a9d0f637be61da9f0dd64e77eadd3878bf0ad
bmwoodruff <bmwoodruff@gmail.com>
2023-01-09 15:11:38 -0700

    after class Mon jan 9

---
+++ 01-09-23.R
@@ -0,0 +1,22 @@
+(-2)^2
+(-1)^2
+seq(-2,5)^2
+vec <- seq(-2,5)
+vec^2
+
+vec <- seq(-2,5,by = 2)
+vec
+vec^2
+
+
+x <- seq(-2,5)
+y <- x^2
+
+plot(x,y)
+plot(x,y,type = "l")
+
+
+
+x <- seq(-2,5,.01)
+y <- x^2
+plot(x,y,type = "l")
